64c2a1d2c21c5af5a53f9b8cef32db37cf85da60
Prashant Sahni <prashant.sahni5@gmail.com>
2013-01-14 18:55:57 +0530

    rails 4 app initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+gem 'rails', :git => "https://github.com/rails/rails.git"
+gem 'uglifier', '>= 1.0.30'
+gem 'activerecord-deprecated_finders', :git => 'git://github.com/rails/activerecord-deprecated_finders.git'
+gem 'journey', :git => 'git://github.com/rails/journey.git'
+
---
+++ testapp_rails4/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestappRails4::Application.config.secret_key_base = 'b99419c47f0ec4cea200eef4cddf73918733f84c90b51a9b772cbcb8fed449a5ffda91874551d2f6781301f1362c0c80175f06a50bf48ceddcb29896a5f72300'
